4fc5300fb36bac32b6b1afe78eed095f270f1c60
Carlos Guzmán <cags84@gmail.com>
2021-03-10 17:47:02 -0500

    Agragegamos segunda clase

---
+++ clase-2-funciones/ejercicio.js
@@ -0,0 +1,13 @@
+/*
+A partir de la siguiente información, construir un array de objetos que contenga la información de cada persona. Luego, calcular cuántos hombres y cuántas mujeres hay en el listado de personas, teniendo en cuenta la información del objeto. 
+
+ID; Nombre; Correo; Edad; Teléfono; Sexo; Dirección
+const db =`
+12345;Juana Perez;juanaperez@gmail.com;20;2345678901;F;Calle 123,
+76543;Juan Rodriguez;jrodriguez@gmail.com;30;2345678901;M;Calle 123,
+231456;Marcia Pumarejo;mpumarejo@gmail.com;25;2345678901;F;Calle 123,
+657546;Carlos Perez;carperez@gmail.com;19;2345678901;M;Calle 123,
+64565;Ana Manrrique;amanrri@gmail.com;9;2345678901;F;Calle 123,
+42342;Pedro Gomez;pgomez@gmail.com;50;2345678901;M;Calle 123
+
+*/
